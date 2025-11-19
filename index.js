4f9e2f426d4550b7c9d78ce40e7c1734d6a7950f
jafarnz <jaf.nz@icloud.com>
2025-11-19 16:16:10 +0800

    Updated index.js to include add tasks.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const { editTask } = require("./utils/updateUtils");
+const { addTask } = require("./utils/createTaskUtil");
 
 //finalise createTasUtil
 
@@ -10,6 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.put("/tasks/:id", editTask);
+app.post("/tasks", addTask);
 
 app.use(express.static(path.join(__dirname, "public")));
 
